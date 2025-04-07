f10131a4577230182b4ec8ad42d43c3ab1d47cd2
Marialafu <lafuentelhmaria@gmail.com>
2025-04-07 11:16:57 +0200

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,62 @@
-const textareaToFill = document.getElementById('textarea-to-fill')
-const excludeSpaceCheckbox = document.getElementById('exclude-spaces-textarea')
-const readingTime = document.getElementById('aprox-reading-time')
+const textareaToFill = document.getElementById('textarea-to-fill');
+const excludeSpaceCheckbox = document.getElementById('exclude-spaces-textarea');
+const readingTime = document.getElementById('aprox-reading-time');
 
-let totalCharactersNumber = document.getElementById ('total-characters-number')
-let wordCountNumber = document.getElementById ('word-count-number')
-let sentenceCountNumber = document.getElementById ('sentence-count-number')
-
+let totalCharactersNumber = document.getElementById('total-characters-number');
+let wordCountNumber = document.getElementById('word-count-number');
+let sentenceCountNumber = document.getElementById('sentence-count-number');
 
 const defineTotalCharactersWithSpaces = () => {
-    let readAmountCharacters = textareaToFill.value.length
-    return readAmountCharacters
-}
+  let readAmountCharacters = textareaToFill.value.length;
+  return readAmountCharacters;
+};
 
 const defineTotalCharacters = () => {
-    let amountCharacters = 0
-    let amountSpaces = 0
+  let amountCharacters = 0;
+  let amountSpaces = 0;
 
-    for (const letter of textareaToFill.value){
-        if (excludeSpaceCheckbox.checked && letter === ' '){
-            amountSpaces++
-            amountCharacters++
-        } else {amountCharacters++}
+  for (const letter of textareaToFill.value) {
+    if (excludeSpaceCheckbox.checked && letter === ' ') {
+      amountSpaces++;
+      amountCharacters++;
+    } else {
+      amountCharacters++;
     }
-    
-    let charactersWithoutSpaces = amountCharacters - amountSpaces
-    return charactersWithoutSpaces
-}
+  }
+
+  let charactersWithoutSpaces = amountCharacters - amountSpaces;
+  return charactersWithoutSpaces;
+};
 
 const defineAmountWords = () => {
-    let words = ''
-    
-    for (const letter of textareaToFill.value){
-       if (letter === '.'){
-        words++
-       } 
-    }
-   
-    if (words === ''){
-        return '0'
-    } else {return words}
-}
+  let amountWords = 0;
+  let words = textareaToFill.value.split(' ');
+  amountWords = words.length;
+  console.log(words);
+
+  return amountWords;
+};
 
 const defineAmountSentences = () => {
-    let sentences = 0
-    for (const letter of textareaToFill.value){
-        if (letter === '.'){
-         sentences++
-        }
-     }
+  let sentences = 0;
+  for (const letter of textareaToFill.value) {
+    if (letter === '.') {
+      sentences++;
+    }
+  }
 
-    //por qué en ternario no me deja
-    if (sentences === ''){
-        return '0'
-    } else {return sentences}
-
-}
-
+  return sentences;
+};
 
 const defineAllCards = () => {
-   
-    if (excludeSpaceCheckbox.checked){
-        totalCharactersNumber.textContent = defineTotalCharacters()
-    } else {
-        totalCharactersNumber.textContent = defineTotalCharactersWithSpaces()
-    }
+  if (excludeSpaceCheckbox.checked) {
+    totalCharactersNumber.textContent = defineTotalCharacters();
+  } else {
+    totalCharactersNumber.textContent = defineTotalCharactersWithSpaces();
+  }
 
-    wordCountNumber.textContent = defineAmountWords()
+  wordCountNumber.textContent = defineAmountWords();
 
-    sentenceCountNumber.textContent = defineAmountSentences()
-    
-}
-textareaToFill.addEventListener('input', defineAllCards)
+  sentenceCountNumber.textContent = defineAmountSentences();
+};
+textareaToFill.addEventListener('input', defineAllCards);
